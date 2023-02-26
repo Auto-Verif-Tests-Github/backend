d56afc74415b3482e2dc632bae39b5e95dc36f35
BogdanHUBIT <b.pyanzin@mail.ru>
2023-02-26 20:32:12 +0700

    f13

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import flask
-import sqlite3
 import csv
+import json
+import sqlite3
+import time
+
+import flask
 
 
-
-#chanfe lastcomm
+# chanfe lastcomm
 def get_hash_password(s):
     return hash(s)
+
+
 def check_hash(cur_hash):
     dbcon = sqlite3.connect('syspro.db')
     dbcur = dbcon.cursor()
@@ -58,6 +62,34 @@ def delete_el(s, args):
     dbcur.close()
     dbcon.close()
 
+
+def get_time():
+    return int(time.time() * 1000)
+
+
+def add_update_solution(args):
+    dbcon = sqlite3.connect('syspro.db')
+    dbcur = dbcon.cursor()
+    params = []
+    num = 0
+
+    for val in args.values():
+        if val is not None:
+            num += 1
+    if num == 2 and 'id' in args and 'status' in args:
+        el_time = get_time()
+        el = {'date': el_time, 'status': args['status']}
+        dbcur.execute(f'SELECT updates FROM solution WHERE id={args["id"]}')
+        updates = dbcur.fetchone()
+        js_obj = json.loads(updates)
+        js_obj['updates'].append(el)
+        dbcur.execute(
+            f'UPDATE solutions SET updates = "{str(js_obj)}", status = "{args["status"]}" WHERE id = {int(args["id"])}')
+        dbcon.commit()
+    else:
+        adding_el_to_table('solutions', args)
+    dbcur.close()
+    dbcon.close()
 def adding_el_to_table(s, args):
     if not(args['hash']) or not(check_hash(args['hash'])):
         flask.abort(403)
@@ -112,7 +144,7 @@ def download_csv_to_db(db, file):
 def create_db(name):
     dbcon = sqlite3.connect(name)
     dbcur = dbcon.cursor()
-    dbcur.execute(f"""CREATE TABLE IF NOT EXISTS people(
+    dbcur.execute("""CREATE TABLE IF NOT EXISTS people(
             id INTEGER ,
             name TEXT,
             github_name TEXT,
@@ -120,20 +152,20 @@ def create_db(name):
             PRIMARY KEY (id),
             FOREIGN KEY (stream_id) REFERENCES streams (id) ON DELETE CASCADE
         )""")
-    dbcur.execute(f"""CREATE TABLE IF NOT EXISTS teachers(
+    dbcur.execute("""CREATE TABLE IF NOT EXISTS teachers(
             id INTEGER ,
             login TEXT UNIQUE,
             password TEXT,
             name TEXT,
             PRIMARY KEY (id)
         )""")
-    dbcur.execute(f"""CREATE TABLE IF NOT EXISTS streams(
+    dbcur.execute("""CREATE TABLE IF NOT EXISTS streams(
             id INTEGER ,
             name TEXT,
             classroom_link TEXT,
             PRIMARY KEY (id)
         )""")
-    dbcur.execute(f"""CREATE TABLE IF NOT EXISTS courses(
+    dbcur.execute("""CREATE TABLE IF NOT EXISTS courses(
             id INTEGER ,
             stream_id INTEGER,
             name TEXT,
@@ -142,7 +174,7 @@ def create_db(name):
             FOREIGN KEY (stream_id) REFERENCES streams (id) ON DELETE CASCADE,
             FOREIGN KEY (teacher_id) REFERENCES teachers (id) ON DELETE CASCADE
         )""")
-    dbcur.execute(f"""CREATE TABLE IF NOT EXISTS tasks(
+    dbcur.execute("""CREATE TABLE IF NOT EXISTS tasks(
             id INTEGER ,
             course_id INTEGER,
             stream_id INTEGER,
@@ -153,13 +185,13 @@ def create_db(name):
             FOREIGN KEY (course_id) REFERENCES courses (id) ON DELETE CASCADE,
             FOREIGN KEY (stream_id) REFERENCES streams (id) ON DELETE CASCADE
         )""")
-    dbcur.execute(f"""CREATE TABLE IF NOT EXISTS solutions(
+    dbcur.execute("""CREATE TABLE IF NOT EXISTS solutions(
             id INTEGER ,
             task_id INTEGER,
             people_id INTEGER,
-            status TEXT,
+            status TEXT NOT NULL DEFAULT "N/A",
             github_link TEXT,
-            last_update TEXT,
+            updates TEXT NOT NULL DEFAULT '{"updates": []}',
             PRIMARY KEY (id),
             FOREIGN KEY (task_id) REFERENCES tasks (id) ON DELETE CASCADE,
             FOREIGN KEY (people_id) REFERENCES people (id) ON DELETE CASCADE
@@ -174,31 +206,30 @@ app = flask.Flask(__name__)
 
 @app.route("/api/courses/",methods=['GET', 'POST'])
 def req_courses():
-    if flask.request.args['del'] and flask.request.method == 'POST':
-        delete_el('courses',flask.request.args.to_dict())
+    if flask.request.args.get('del') and flask.request.form == 'POST':
+        delete_el('courses', flask.request.args.to_dict())
     elif flask.request.method == 'GET':
         return calc_request('courses', flask.request.args.to_dict())
-    elif flask.request.method == 'POST':
+    elif flask.request.form == 'POST':
         adding_el_to_table('courses', flask.request.args.to_dict())
     return str({"response": 1})
 
 @app.route("/api/streams/",methods=['GET', 'POST'])
 def req_streams():
-    if flask.request.args['del'] and flask.request.method == 'POST':
-        delete_el('streams',flask.request.args.to_dict())
+    if flask.request.args.get('del') and flask.request.form == 'POST':
+        delete_el('streams', flask.request.args.to_dict())
     elif flask.request.method == 'GET':
         return calc_request('streams', flask.request.args.to_dict())
-    elif flask.request.method == 'POST':
+    elif flask.request.form == 'POST':
         adding_el_to_table('streams', flask.request.args.to_dict())
     return str({"response": 1})
-#@app.route("/api/streams/?del=1")
 @app.route("/api/people/",methods=['GET', 'POST'])
 def req_people():
-    if flask.request.args['del'] and flask.request.method == 'POST':
-        delete_el('people',flask.request.args.to_dict())
+    if flask.request.args.get('del') and flask.request.form == 'POST':
+        delete_el('people', flask.request.args.to_dict())
     elif flask.request.method == 'GET':
         return calc_request('people', flask.request.args.to_dict())
-    elif flask.request.method == 'POST':
+    elif flask.request.form == 'POST':
         adding_el_to_table('people', flask.request.args.to_dict())
     return str({"response": 1})
 
@@ -206,32 +237,32 @@ def req_people():
 def req_teachers():
     if flask.request.args['password']:
         flask.request.args['password']=get_hash_password(flask.request.args['password'])
-    if flask.request.args['del'] and flask.request.method == 'POST':
-        delete_el('teachers',flask.request.args.to_dict())
+    if flask.request.args.get('del') and flask.request.form == 'POST':
+        delete_el('teachers', flask.request.args.to_dict())
     elif flask.request.method == 'GET':
         return calc_request('teachers', flask.request.args.to_dict())
-    elif flask.request.method == 'POST':
+    elif flask.request.form == 'POST':
         adding_el_to_table('teachers', flask.request.args.to_dict())
     return str({"response": 1})
 @app.route("/api/tasks/",methods=['GET', 'POST'])
 def req_tasks():
-    if flask.request.args['del'] and flask.request.method == 'POST':
+    if flask.request.args.get('del') and flask.request.form == 'POST':
         delete_el('tasks', flask.request.args.to_dict())
     elif flask.request.method == 'GET':
         return calc_request('tasks', flask.request.args.to_dict())
-    elif flask.request.method == 'POST':
+    elif flask.request.form == 'POST':
         adding_el_to_table('tasks', flask.request.args.to_dict())
     return str({"response": 1})
 
 
 @app.route("/api/solutions/",methods=['GET', 'POST'])
 def req_solutions():
-    if flask.request.args['del'] and flask.request.method == 'POST':
-        delete_el('solutions',flask.request.args.to_dict())
+    if flask.request.args.get('del') and flask.request.form == 'POST':
+        delete_el('solutions', flask.request.args.to_dict())
     elif flask.request.method == 'GET':
         return calc_request('solutions', flask.request.args.to_dict())
-    elif flask.request.method == 'POST':
-        adding_el_to_table('solutions', flask.request.args.to_dict())
+    elif flask.request.form == 'POST':
+        add_update_solution(flask.request.args.to_dict())
     return str({"response": 1})
 
 
